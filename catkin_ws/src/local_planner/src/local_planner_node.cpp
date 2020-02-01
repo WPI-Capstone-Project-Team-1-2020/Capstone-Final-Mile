9ca0bc0cfffb0b6f94ae6c2f6af3f76e69af61bc
John Blankenhorn <jrblankenhorn@wpi.edu>
2020-02-01 16:34:31 -0500

    initial local planner setup stuff

---
+++ catkin_ws/src/local_planner/src/local_planner_node.cpp
@@ -0,0 +1,20 @@
+
+#include <ros/ros.h>
+#include <local_planner.hpp>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "local_planner_node");
+
+    ros::NodeHandle nh;
+
+    ros::NodeHandle pnh("~");
+
+    while(ros::ok)
+    {
+        ros::spin();
+    }
+
+    return 0;
+
+}

dff62f12626586ff55a5963359f748a030daf95d
jep89 <julae5@gmail.com>
2020-03-03 21:35:05 -0500

    Add files via upload

---
+++ catkin_ws/src/hector_quadrotor/hector_quadrotor_model/matlab/codegen/lib/quadrotorDrag/quadrotorDrag_terminate.c
@@ -0,0 +1,22 @@
+/*
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ *
+ * quadrotorDrag_terminate.c
+ *
+ * Code generation for function 'quadrotorDrag_terminate'
+ *
+ */
+
+/* Include files */
+#include "quadrotorDrag_terminate.h"
+#include "quadrotorDrag.h"
+
+/* Function Definitions */
+void quadrotorDrag_terminate(void)
+{
+  /* (no terminate code required) */
+}
+
+/* End of code generation (quadrotorDrag_terminate.c) */
---
+++ catkin_ws/src/hector_quadrotor/hector_quadrotor_model/matlab/codegen/lib/quadrotorDrag/quadrotorDrag_terminate.h
@@ -0,0 +1,26 @@
+/*
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ *
+ * quadrotorDrag_terminate.h
+ *
+ * Code generation for function 'quadrotorDrag_terminate'
+ *
+ */
+
+#ifndef QUADROTORDRAG_TERMINATE_H
+#define QUADROTORDRAG_TERMINATE_H
+
+/* Include files */
+#include <stddef.h>
+#include <stdlib.h>
+#include "rtwtypes.h"
+#include "quadrotorDrag_types.h"
+
+/* Function Declarations */
+extern void quadrotorDrag_terminate(void);
+
+#endif
+
+/* End of code generation (quadrotorDrag_terminate.h) */
